883320f4a2f6c34de4958c587ce082e690465072
sahil-sahu <sahilku2003@gmail.com>
2022-01-18 20:27:25 +0530

    timepass

--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,7 @@ class MyDocument extends Document {
         <Head>
             <link rel="icon" href="/favicon.png" type="image/x-icon" />  
             <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
             <link href="https://fonts.googleapis.com/css2?family=Jost:wght@100;200;300;400;500;600;700;800&display=swap" rel="stylesheet" /> 
         </Head>
         <body>
