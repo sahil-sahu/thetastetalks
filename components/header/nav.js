0dfa45836aa46b84cf095ccd4d152fcfb7eb54e8
sahil-sahu <sahilku2003@gmail.com>
2022-02-03 08:58:37 +0530

    made the bottom nav

--- components/header/nav.js
+++ components/header/nav.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import styles from './nav.module.css'
 import Link from 'next/link'
 
@@ -10,20 +11,44 @@ function Nav(props){
     }
 
     var navItems = [
-        {"name": "Restaurants", "slug":"/restaurants"},
-        {"name": "Recipes", "slug":"/recipes"},
-        {"name": "Home", "slug":"/"},
-        {"name": "Chefs", "slug":"/chefs"},
-        {"name": "Food Bloggers", "slug":"/bloggers"},
-        {"name": "Meet-ups", "slug":"/meetups"}
+        {"name": "Restaurants", "slug":"/restaurants","icon":"/assets/navIcons/restaurant.png"},
+        {"name": "Recipes", "slug":"/recipes","icon":"/assets/navIcons/recipes.png"},
+        {"name": "Home", "slug":"/","icon":"/assets/navIcons/home.png"},
+        {"name": "Chefs", "slug":"/chefs","icon":"/assets/navIcons/chef.png"},
+        {"name": "Bloggers", "slug":"/bloggers","icon":"/assets/navIcons/blogging.png"},
+        {"name": "MeetUps", "slug":"/meetups","icon":"/assets/navIcons/meetup.png"}
     ]
-    delete navItems[props.omit]
+    const size = useWindowSize();
+
+    if (size.width > 600){
+        delete navItems[props.omit]
+    }
+
     return (
 
         <nav className={`${styles.nav} ${bar}`} >
+            <style>{`
+                :root{
+                    --navID : ${props.omit};
+                }`}
+            </style>
+            <style>
+                {
+                `    
+                .${styles.nav} ul li:nth-child(${props.omit+1}) span{
+                    opacity: 1;
+                }
+                .${styles.nav} ul li:nth-child(${props.omit+1}) img{
+                    transform: translateY(-1.2rem)
+                }
+                `}
+            </style>
             <ul>
                 {navItems.map((object, i)=>{
-                    return(<li><Link href={object.slug}><a>{object.name}</a></Link></li>)
+                    return(<li><Link href={object.slug}><a>
+                        <img src={object.icon} alt="" />
+                        <span>{object.name}</span>
+                        </a></Link></li>)
                 }) } 
             </ul>
         </nav>
@@ -31,5 +56,26 @@ function Nav(props){
     )
 
 }
+
+function useWindowSize() {
+    const [windowSize, setWindowSize] = useState({
+      width: undefined,
+    });
+  
+    useEffect(() => {
+      if (typeof window !== 'undefined') {
+        function handleResize() {
+          setWindowSize({
+            width: window.innerWidth,
+          });
+        }
+      
+        window.addEventListener("resize", handleResize);
+        handleResize();
+        return () => window.removeEventListener("resize", handleResize);
+      }
+    }, []);
+    return windowSize;
+  }
 
 export default Nav;
